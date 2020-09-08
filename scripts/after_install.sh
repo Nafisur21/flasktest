b0ca2cf2ea957c9b19b99e5bf8699c6ac37972e8
nafisur <nafisur.rahman21@gmail.com>
2020-09-08 21:34:42 +0530

    gunicorn

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 BASEDIR=/home/ec2-user/opt/flasktest
 . $BASEDIR/venv/bin/activate
-pip install -r /home/ec2-user/opt/flasktestapp/requirements.txt
+pip install -r $BASEDIR/requirements.txt
+pkill -9 gunicorn
