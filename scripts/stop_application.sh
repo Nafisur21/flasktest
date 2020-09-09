a6f79748a000087c892d6f005b551a9b5a5e816c
nafisur <nafisur.rahman21@gmail.com>
2020-09-09 11:24:07 +0530

    new appspec

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/www/project/
-source /home/ec2-user/www/project-venv/bin/activate
-sudo pkill gunicorn
+#cd /home/ec2-user/www/project/
+#source /home/ec2-user/www/project-venv/bin/activate
+#sudo pkill gunicorn
