a47346083c86b93c15378d23fb490f793626879c
nafisur <nafisur.rahman21@gmail.com>
2020-09-09 12:10:25 +0530

    app stop

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
-#cd /home/ec2-user/www/project/
-#source /home/ec2-user/www/project-venv/bin/activate
-#sudo pkill gunicorn
+#!/bin/bash
+cd /home/ec2-user/www/project/
+source /home/ec2-user/www/venv/bin/activate
+sudo pkill gunicorn
